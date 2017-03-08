4271d32750a9976514816792390f66b0db2c25c1
exxaxxxx <mr.black011@gmail.com>
2017-03-08 16:24:05 +0100

    Final version. I hope….

--- src/blueteam/TeamConstants.java
+++ src/blueteam/TeamConstants.java
@@ -3,7 +3,7 @@ package blueteam;
 import battlecode.common.Direction;
 
 public abstract interface TeamConstants {
-	public static final int DESIRED_NUMBER_OF_GARDENERS = 8;
+	public static final int DESIRED_NUMBER_OF_GARDENERS = 6;
 	public static final int GARDENERS_COUNT_CHANNEL = 0;
 
 	// The maximum number of tries per turn for finding a direction in which
@@ -29,7 +29,7 @@ public abstract interface TeamConstants {
 	public static final int MINIMUM_BULLETS_TO_SAVE = 150;
 
 	public static final int GARDENERS_DIRECT_PATH_LENGTH = 30;
-	public static final float GARDENERS_DEFAULT_FREE_SPOT_RADIUS = 5f;
+	public static final float GARDENERS_DEFAULT_FREE_SPOT_RADIUS = 6.5f;
 
 	public static final Direction GARDENERS_GARDEN_ENTRANCE = Direction.WEST;
 	public static final int GARDENER_NUM_OF_TREES_TO_BUILD_LUMBER = 10;
