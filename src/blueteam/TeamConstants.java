f8940a0a3860dbbecdde4edaf7d95864e231b380
Formicarufa <Formicarufa@users.noreply.github.com>
2017-03-12 19:53:22 +0100

    Triple shots and combat locations sharing

--- src/blueteam/TeamConstants.java
+++ src/blueteam/TeamConstants.java
@@ -12,7 +12,7 @@ public abstract interface TeamConstants {
 	// Probability that the moving direction of a soldier is chosen at random.
 	// (Otherwise the soldier tries to move towards the enemy archon's initial
 	// location.)
-	public static final double SOLDIER_RANDOM_MOVE_PROB = .75;
+	public static final double SOLDIER_RANDOM_MOVE_PROB = .55;
 	/**
 	 * robots having less than MINIMUM_HEALTH_PERCENTAGE HP are considered dead
 	 */
@@ -37,4 +37,19 @@ public abstract interface TeamConstants {
 
 	public static final int MAX_NUMBER_LUMBERJACKS = 3;
 	public static final int MAX_NUMBER_SCOUTS = 2;
+
+	/**
+	 * The soldier will not use triad shots if the number of bullets is below
+	 * this value. Solves the issue that a single soldier was able to prevent
+	 * the whole team from development by shooting (see map Conga)
+	 */
+
+	public static final int MINIMUM_BULLETS_TO_SAVE_BY_SOLDIER = 70;
+
+	/**
+	 * Maximal distance from an ENEMY soldier/tank, the robot will not try to
+	 * get closer than this distance (reason: have enough time to dodge).
+	 */
+	public static final int MAX_SOLDIER_TO_SOLDIER_DISTANCE = 5;
+	public static final int COMBAT_LOCATIONS_FIRST_CHANNEL = 100;
 }
